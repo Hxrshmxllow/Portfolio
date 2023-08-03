851d1c7e55b930c7a9db33ddb4ef9e30d4203caa
Hxrshmxllow <86836424+Hxrshmxllow@users.noreply.github.com>
2023-08-03 17:00:31 +0000

    yes

--- E-Commerce Website/app.py
+++ E-Commerce Website/app.py
@@ -2,9 +2,25 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
+class Product():          # leave this empty
+    def __init__(self, id, name, price, imageFile, desc, sizes, qty):   # constructor function using self
+        self.Id = id  # variable using self.
+        self.Name = name  # variable using self
+        self.Price = price  # variable using self
+        self.ImageFile = imageFile  # variable using self
+        self.Description = desc  # variable using self
+        self.Sizes = sizes  # variable using self
+        self.StockQty = qty  # variable using self
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    prodArray = []
+
+    for x in range(8):
+        prodOj = Product(x+1, "Product "+str(x+1), x + 10, "card" +str(x+1) +".png", "Product Desc", 10, 15)
+        prodArray.append(prodOj)
+
+    return render_template('index.html', products=prodArray)
 
 @app.route("/search", methods = ['GET', 'POST'])
 def search():
